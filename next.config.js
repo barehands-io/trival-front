b07f6e02a33bf70f43c1137faee0456e56f266ee
barehands-io <>
2023-01-26 07:19:04 +0000

    updated fun fact page

--- next.config.js
+++ next.config.js
@@ -1,11 +1,17 @@
 /** @type {import('next').NextConfig} */
 
+
+
 // import env library from 'dotenv'
 
 // load env variables from .env file
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+    images:{
+        domains: ['res.cloudinary.com'],
+
+    }
 };
 
 module.exports = nextConfig;
--- pages/fun-facts.tsx
+++ pages/fun-facts.tsx
@@ -1,62 +1,103 @@
-import {useState} from "react";
+import { useState } from "react";
+import Image from "next/image";
 
+interface FunFactProps {
+  fact: string;
+  imageUrl: string;
+  imageName: string;
+}
 
 interface Props {
-    data: any;
-    endpointUrl: string;
+  data: FunFactProps;
+  endpointUrl: string;
 }
 
 const fetchFunFact = async (endpointUrl: string) => {
-    try {
-        const res = await fetch(`${endpointUrl}/api/fun-facts`);
-        return (await res.json()).data;
-    } catch (error) {
-        console.error(error);
-        return '';
-    }
+  try {
+    const res = await fetch(`${endpointUrl}/api/fun-facts`);
+    return (await res.json()).data;
+  } catch (error) {
+    console.error(error);
+    return "";
+  }
 };
 
 export const getStaticProps = async () => {
-    const endpointUrl = process.env.APP_ENDPOINT_URL as string;
-    const data = await fetchFunFact(endpointUrl);
-    return {
-        props: {
-            data,
-            endpointUrl
-        },
-    };
+  const endpointUrl = process.env.APP_ENDPOINT_URL as string;
+  const data = await fetchFunFact(endpointUrl);
+  return {
+    props: {
+      data,
+      endpointUrl,
+    },
+  };
 };
 
-export default function FunFactPage({data, endpointUrl}: Props) {
-    const [funFact, setFunFact] = useState(data);
-    const [loading, setLoading] = useState(false);
-    const handleRegenerate = async () => {
-        setLoading(true);
-        const newData = await fetchFunFact(endpointUrl);
+export default function FunFactPage({ data, endpointUrl }: Props) {
+  const dummy = {
+    fact: "\n\nThe Galapagos Islands are home to some of the highest levels of endemic species in the world, with nearly 70% of the land mammals and 30% of the plant species found nowhere else on Earth!",
+    imageUrl:
+      "https://res.cloudinary.com/dmjluoznt/image/upload/v1674715214/_Galapagos_Islands_Endemic_Species.jpg.png",
+    imageName: "_Galapagos_Islands_Endemic_Species.jpg",
+  };
+  const [funFact, setFunFact] = useState(data);
+  const [loading, setLoading] = useState(false);
+  const handleRegenerate = async () => {
+    setLoading(true);
+    const newData = await fetchFunFact(endpointUrl);
 
-        console.log('regenerate', newData);
+    console.log("regenerate", newData);
 
-        setFunFact(newData);
-        setLoading(false);
-    }
+    setFunFact(newData);
+    setLoading(false);
+  };
 
-    return (
+  return (
+    <div
+      style={{
+        marginTop: "72px",
+        width: "100%",
+        position: "relative",
+        overflow: "hidden",
+        background:
+          "radial-gradient(circle, rgba(2, 11, 21, 1) 0%, rgba(8, 61, 85, 1) 76%)",
+      }}
+    >
+      {loading ? (
+        <div>Loading...</div>
+      ) : (
         <div>
-            {
-                loading ? <div>Loading...</div> : <div>
-                    <div>
-                        <div className="pt-20 text-center">
-                            <h1 className="text-xl py-6">Here is a Fun Fact you probably didnt know</h1>
-                            <h1 className="text-xl">  {funFact}</h1>
-                        </div>
-                    </div>
+          <div>
+            <div className="container relative mx-auto pt-20 text-center">
+              <div className="">
+                <h1 className="py-6 text-xl">
+                  Here is a Fun Fact you probably didnt know
+                </h1>
+                <h1 className="text-xl"> {funFact.fact}</h1>
+              </div>
 
-                    <div className="flex justify-center my-10">
-                        <button onClick={handleRegenerate} className="bg-red-500 p-2 rounded-md"> Regenerate</button>
-                    </div>
-                </div>
-            }
+              <div className="flex justify-center rounded-lg py-10">
+                <img
+                  className="h-[400px] rounded-lg"
+                  alt={funFact.imageName}
+                  src={funFact.imageUrl}
+                />
+              </div>
+              {/*<Image width={200} height={300} src={funFact.imageUrl} alt={funFact.imageName}/>*/}
+            </div>
+          </div>
 
+          <div className="my-10 flex justify-center">
+            <button
+              onClick={handleRegenerate}
+              className="rounded-md bg-red-500 p-2"
+            >
+              {" "}
+              Regenerate
+            </button>
+          </div>
         </div>
-    );
+      )}
+    </div>
+  );
 }
